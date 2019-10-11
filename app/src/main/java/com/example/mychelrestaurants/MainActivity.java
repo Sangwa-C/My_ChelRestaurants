3734fb8f83e682c264cd11961583f640777e117f
Sangwa-C <chelseasabinesangwa@gmail.com>
2019-10-11 11:05:18 +0200

    edits

---
+++ app/src/main/java/com/example/mychelrestaurants/MainActivity.java
@@ -0,0 +1,43 @@
+package com.example.mychelrestaurants;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+//import android.support.v7.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private static final String TAG = MainActivity.class.getSimpleName();
+    @BindView(R.id.findRestaurantsButton) Button mFindRestaurantsButton;
+    @BindView(R.id.locationEditText) EditText mLocationEditText;
+//    @BindView(R.id.appNameTextView) TextView mAppNameTextView;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        mFindRestaurantsButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v == mFindRestaurantsButton) {
+            String location = mLocationEditText.getText().toString();
+            Intent intent = new Intent(MainActivity.this, RestaurantsActivity.class);
+            intent.putExtra("location", location);
+            startActivity(intent);
+        }
+    }
+}
+
+// Toast.makeText(MainActivity.this, "Wait a bit!", Toast.LENGTH_LONG).show();
