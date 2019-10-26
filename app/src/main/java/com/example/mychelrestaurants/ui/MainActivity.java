bee714d262d1a818be01718351b498de84e710ac
Sangwa-C <chelseasabinesangwa@gmail.com>
2019-10-26 16:49:50 +0200

    edits on the main activity

--- app/src/main/java/com/example/mychelrestaurants/ui/MainActivity.java
+++ app/src/main/java/com/example/mychelrestaurants/ui/MainActivity.java
@@ -2,12 +2,20 @@ package com.example.mychelrestaurants.ui;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.mychelrestaurants.Constants;
 import com.example.mychelrestaurants.R;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -15,16 +23,52 @@ import butterknife.ButterKnife;
 //import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-    @BindView(R.id.findRestaurantsButton) Button mFindRestaurantsButton;
+//    private SharedPreferences mSharedPreferences;
+//    private SharedPreferences.Editor mEditor;
 
+    private DatabaseReference mSearchedLocationReference;
+    private ValueEventListener mSearchedLocationReferenceListener;
+
+    @BindView(R.id.findRestaurantsButton) Button mFindRestaurantsButton;
+    @BindView(R.id.locationEditText)
+    EditText mLocationEditText;
     @BindView(R.id.savedRestaurantsButton) Button mSavedRestaurantsButton;
+
+//    @BindView(R.id.restaurantNameTextView) TextView mAppNameTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        mSearchedLocationReference = FirebaseDatabase
+                .getInstance()
+                .getReference()
+                .child(Constants.FIREBASE_CHILD_SEARCHED_LOCATION);//pinpoint location node
+        mSearchedLocationReferenceListener = mSearchedLocationReference.addValueEventListener(new ValueEventListener() {
+
+
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {//something changed!
+                for (DataSnapshot locationSnapshot : dataSnapshot.getChildren()) {
+                    String location = locationSnapshot.getValue().toString();//   code here is executed when Firebase recognizes a change made to the
+                    Log.d("Locations updated", "location: " + location);  //   node being listened to, and provides a new dataSnapshot.
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) { //update UI here if error occurred.
+
+            }
+
+        });
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
 
+//        Typeface ostrichFont = Typeface.createFromAsset(getAssets(), "fonts/ostrich-regular.ttf");
+//        mAppNameTextView.setTypeface(ostrichFont);
+
+//        mSharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+//        mEditor = mSharedPreferences.edit();
 
         mFindRestaurantsButton.setOnClickListener(this);
         mSavedRestaurantsButton.setOnClickListener(this);
@@ -32,15 +76,38 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     @Override
     public void onClick(View v) {
-
         if(v == mFindRestaurantsButton) {
+            String location = mLocationEditText.getText().toString();
+
+            saveLocationToFirebase(location);
+
+//            if(!(location).equals("")) {
+//                addToSharedPreferences(location);
+//            }
+
             Intent intent = new Intent(MainActivity.this, RestaurantsListActivity.class);
+            intent.putExtra("location", location);
             startActivity(intent);
         }
-
         if (v == mSavedRestaurantsButton) {
             Intent intent = new Intent(MainActivity.this, SavedRestaurantListActivity.class);
             startActivity(intent);
         }
+
     }
-}// Toast.makeText(MainActivity.this, "Wait a bit!", Toast.LENGTH_LONG).show();
+
+    public void saveLocationToFirebase(String location) {
+        mSearchedLocationReference.push().setValue(location);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy(); //    defined in 'top level' of activity, not nested within another block.
+        mSearchedLocationReference.removeEventListener(mSearchedLocationReferenceListener);  //    code here is executed when the user quits the activity.
+    }
+
+//    private void addToSharedPreferences(String location) {
+//        mEditor.putString(Constants.PREFERENCES_LOCATION_KEY, location).apply();
+//    }
+
+}
